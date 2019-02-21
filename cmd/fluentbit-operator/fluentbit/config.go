151cddd7ba261dd3566cc964e0c9f4e31e53ebab
root <danma@yunify.com>
2019-02-21 11:20:42 +0800

    Simplify kubernetes log message

--- cmd/fluentbit-operator/fluentbit/config.go
+++ cmd/fluentbit-operator/fluentbit/config.go
@@ -18,12 +18,52 @@ var fluentBitConfigTemplate = `
     DB               /tail-db/tail-containers-state.db
     DB.Sync          Normal
 
-[FILTER]
+    [FILTER]
     Name                kubernetes
     Match               kube.*
     Kube_URL            https://kubernetes.default.svc:443
     Kube_CA_File        /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
     Kube_Token_File     /var/run/secrets/kubernetes.io/serviceaccount/token
+
+[FILTER]
+    Name                nest
+    Match               kube.*
+    Operation           lift
+    Nested_under        kubernetes
+    Prefix_with         kubernetes_
+
+[FILTER]
+    Name                modify
+    Match               kube.*
+    Remove              stream
+
+[FILTER]
+    Name                modify
+    Match               kube.*
+    Remove              kubernetes_labels
+
+[FILTER]
+    Name                modify
+    Match               kube.*
+    Remove              kubernetes_annotations
+
+[FILTER]
+    Name                modify
+    Match               kube.*
+    Remove              kubernetes_pod_id
+
+[FILTER]
+    Name                modify
+    Match               kube.*
+    Remove              kubernetes_docker_id
+
+[FILTER]
+    Name                nest
+    Match               kube.*
+    Operation           nest
+    Wildcard            kubernetes_*
+    Nested_under        kubernetes
+    Remove_prefix       kubernetes_
 
 [OUTPUT]
     Name  null
