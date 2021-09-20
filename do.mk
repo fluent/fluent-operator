fd754ee7723e7fe6779db20fef530dcfb448bf59
Bojan <dbojan@gmail.com>
2021-09-20 19:52:04 -0300

    add support for custom fluent bit image

--- do.mk
+++ do.mk
@@ -1,6 +1,7 @@
 REV ?= $(shell git rev-parse --short HEAD)
 PREFIX ?= do-
 IMAGE_TAG ?= $(REV:$(PREFIX)%=%)
+FB_VERSION ?= 1.8.7
 
 ifdef release
 	REV = $(shell git rev-list --tags --max-count=1)
@@ -22,4 +23,18 @@ image-push-operator: image-operator
 	docker push digitaloceanapps/fluent-bit-operator:$(IMAGE_TAG)
 ifdef latest
 	docker push digitaloceanapps/fluent-bit-operator:latest
+endif
+
+.PHONY: image-fluentbit
+image-fluentbit:
+	docker build --build-arg fluentbitversion=${FB_VERSION} -f cmd/fluent-bit-watcher/Dockerfile -t digitaloceanapps/fluent-bit:${FB_VERSION}-$(IMAGE_TAG) .
+ifdef latest
+	docker tag digitaloceanapps/fluent-bit::${FB_VERSION}-$(IMAGE_TAG) digitaloceanapps/fluent-bit:latest
+endif
+
+.PHONY: image-push-fluentbit
+image-push-fluentbit: image-fluentbit
+	docker push digitaloceanapps/fluent-bit:${FB_VERSION}-$(IMAGE_TAG)
+ifdef latest
+	docker push digitaloceanapps/fluent-bit:latest
 endif
