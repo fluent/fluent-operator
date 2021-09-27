77d75aa6b9d20d237508dba5fced418394b7a26b
root <dehaocheng@yunify.com>
2021-09-27 13:51:52 +0800

    Upgrade layout from Kubebuilder v2 to Kubebuilder v3.1
    Signed-off-by: chengdehao <dehaocheng@yunify.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -1,4 +1,5 @@
 /*
+Copyright 2021.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,23 +18,25 @@ package main
 
 import (
 	"flag"
+	"github.com/joho/godotenv"
 	"os"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"strings"
 
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
 	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	loggingv1alpha2 "kubesphere.io/fluentbit-operator/api/fluentbitoperator/v1alpha2"
-	"kubesphere.io/fluentbit-operator/controllers"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	// required for generating code from CRD
-	_ "k8s.io/code-generator/cmd/client-gen/generators"
-
-	// +kubebuilder:scaffold:imports
-	"github.com/joho/godotenv"
+	loggingv1alpha2 "kubesphere.io/fluentbit-operator/api/fluentbitoperator/v1alpha2"
+	"kubesphere.io/fluentbit-operator/controllers"
+	//+kubebuilder:scaffold:imports
 )
 
 var (
@@ -42,50 +45,55 @@ var (
 )
 
 func init() {
-	_ = clientgoscheme.AddToScheme(scheme)
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	_ = loggingv1alpha2.AddToScheme(scheme)
-	// +kubebuilder:scaffold:scheme
+	utilruntime.Must(loggingv1alpha2.AddToScheme(scheme))
+	//+kubebuilder:scaffold:scheme
 }
 
 func main() {
 	var metricsAddr string
-	var watchNamespaces string
 	var enableLeaderElection bool
+	var probeAddr string
+	var watchNamespaces string
 	var logPath string
 	flag.StringVar(&watchNamespaces, "watch-namespaces", "", "Optional comma separated list of namespaces to watch for resources in. Defaults to cluster scope.")
-	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
-		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
 	if envs, err := godotenv.Read("/fluentbit-operator/fluent-bit.env"); err == nil {
 		logPath = envs["CONTAINER_ROOT_DIR"] + "/containers"
 	}
 
-	ctrl.SetLogger(zap.New(func(o *zap.Options) {
-		o.Development = true
-	}))
-
-	opts := ctrl.Options{
-		Scheme:             scheme,
-		MetricsBindAddress: metricsAddr,
-		LeaderElection:     enableLeaderElection,
-		Port:               9443,
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	ctrlOpts := ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "45c4fdd2.kubesphere.io",
 	}
-
 	namespacedController := false
 	if watchNamespaces != "" {
 		namespacedController = true
 		namespaces := strings.Split(watchNamespaces, ",")
 		if len(namespaces) > 1 {
-			opts.NewCache = cache.MultiNamespacedCacheBuilder(namespaces)
+			ctrlOpts.NewCache = cache.MultiNamespacedCacheBuilder(namespaces)
 		} else {
-			opts.Namespace = namespaces[0]
+			ctrlOpts.Namespace = namespaces[0]
 		}
 	}
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), opts)
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrlOpts)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
@@ -110,7 +118,16 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "FluentBit")
 		os.Exit(1)
 	}
-	// +kubebuilder:scaffold:builder
+	//+kubebuilder:scaffold:builder
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
 
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
