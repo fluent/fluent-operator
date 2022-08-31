35ce4fb047c1c4a07133cb653a31e1a96616a843
tl <tonyzaizai@users.noreply.github.com>
2022-08-31 16:39:15 +0800

    fix: Fluent-bit supports the hostNetwork configuration of pod (#369)
    * fix: Fluent-bit supports the hostNetwork configuration of pod
    
    Signed-off-by: tl <tonyzaizai@kubesphere.io>
    
    * fix: Specification of edge scenario YAML profile content
    
    Signed-off-by: tl <tonyzaizai@kubesphere.io>
    
    * fix: Specification of edge scenario YAML profile content again
    
    Signed-off-by: tl <tonyzaizai@kubesphere.io>
    
    Signed-off-by: tl <tonyzaizai@kubesphere.io>

--- apis/fluentbit/v1alpha2/plugins/input/prometheus_scrape_metrics_type.go
+++ apis/fluentbit/v1alpha2/plugins/input/prometheus_scrape_metrics_type.go
@@ -2,11 +2,9 @@ package input
 
 import (
 	"fmt"
-
-	"strings"
-
 	"github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins"
 	"github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/params"
+	"strings"
 )
 
 // +kubebuilder:object:generate:=true
@@ -41,7 +39,7 @@ func (p *PrometheusScrapeMetrics) Params(_ plugins.SecretLoader) (*params.KVs, e
 		kvs.Insert("tag", p.Tag)
 	}
 	host := strings.ToLower(p.Host)
-	if host == "" || host == "localhost" || host == "127.0.0.1" || host == "host" {
+	if host == "" || host == "host" {
 		kvs.Insert("host", "${HOST_IP}")
 	} else {
 		kvs.Insert("host", p.Host)
