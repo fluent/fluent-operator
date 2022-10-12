432765d6722417a910b4441d8b3b481deea14668
Jan Nemcik <jan.nemcik@solargis.com>
2022-10-12 14:50:44 +0200

    updated docs and manifests for cluster parser
    Signed-off-by: Jan Nemcik <janynemcik5@gmail.com>

--- apis/fluentbit/v1alpha2/plugins/parser/regex_types.go
+++ apis/fluentbit/v1alpha2/plugins/parser/regex_types.go
@@ -17,10 +17,10 @@ type Regex struct {
 	// Time_Format, eg. %Y-%m-%dT%H:%M:%S %z
 	TimeFormat string `json:"timeFormat,omitempty"`
 	// Time_Keep
-	TimeKeep *bool  `json:"timeKeep,omitempty"`
-	// Time_Offset
-	TimeOffset *bool  `json:"timeOffset,omitempty"`
-	Types    string `json:"types,omitempty"`
+	TimeKeep *bool `json:"timeKeep,omitempty"`
+	// Time_Offset, eg. +0200
+	TimeOffset string `json:"timeOffset,omitempty"`
+	Types      string `json:"types,omitempty"`
 }
 
 func (_ *Regex) Name() string {
@@ -41,11 +41,11 @@ func (re *Regex) Params(_ plugins.SecretLoader) (*params.KVs, error) {
 	if re.TimeKeep != nil {
 		kvs.Insert("Time_Keep", fmt.Sprint(*re.TimeKeep))
 	}
-	if re.TimeKeep != nil {
-		kvs.Insert("Time_Keep", fmt.Sprint(*re.TimeKeep))
+	if re.TimeOffset != "" {
+		kvs.Insert("Time_Offset", re.TimeOffset)
 	}
 	if re.Types != "" {
-		kvs.Insert("Time_Offset", fmt.Sprint(*re.TimeOffset))
+		kvs.Insert("Types", re.Types)
 	}
 	return kvs, nil
 }
