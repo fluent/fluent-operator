ff28a2e147d88213b9d6b43fc008344c51a03641
Michael J. Wieneke <m.wieneke@sap.com>
2022-11-07 13:45:04 +0100

    moving Retry_Limit to commons instead of only http-output
    Signed-off-by: Michael J. Wieneke <m.wieneke@sap.com>

--- apis/fluentbit/v1alpha2/plugins/common_types.go
+++ apis/fluentbit/v1alpha2/plugins/common_types.go
@@ -1,6 +1,8 @@
 package plugins
 
-import "github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/params"
+import (
+	"github.com/fluent/fluent-operator/apis/fluentbit/v1alpha2/plugins/params"
+)
 
 // +kubebuilder:object:generate:=true
 
@@ -8,11 +10,17 @@ type CommonParams struct {
 
 	// Alias for the plugin
 	Alias string `json:"alias,omitempty"`
+	// Integer value to set the maximum number of retries allowed. N must be >= 1
+	// (default: 2) or false for no limit at all.
+	RetryLimit string `json:"retryLimit,omitempty"`
 }
 
 func (c *CommonParams) AddCommonParams(kvs *params.KVs) error {
 	if c.Alias != "" {
 		kvs.Insert("Alias", c.Alias)
+	}
+	if c.RetryLimit != "" {
+		kvs.Insert("Retry_Limit", c.RetryLimit)
 	}
 	return nil
 }
