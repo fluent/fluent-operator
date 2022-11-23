94e6449703f633eccc19e01f21e0e0c25444b212
Michael J. Wieneke <m.wieneke@sap.com>
2022-11-23 14:21:40 +0100

    feat: adding support for labels for fluent-operator and fluent-bit pods
    Signed-off-by: Michael J. Wieneke <m.wieneke@sap.com>

--- apis/fluentbit/v1alpha2/fluentbit_types.go
+++ apis/fluentbit/v1alpha2/fluentbit_types.go
@@ -67,6 +67,8 @@ type FluentBitSpec struct {
 	VolumesMounts []corev1.VolumeMount `json:"volumesMounts,omitempty"`
 	// Annotations to add to each Fluentbit pod.
 	Annotations map[string]string `json:"annotations,omitempty"`
+	// Labels to add to each FluentBit pod
+	Labels map[string]string `json:"labels,omitempty"`
 	// SecurityContext holds pod-level security attributes and common container settings.
 	SecurityContext *corev1.PodSecurityContext `json:"securityContext,omitempty"`
 	// Host networking is requested for this pod. Use the host's network namespace. If this option is set, the ports that will be used must be specified. Default to false.
