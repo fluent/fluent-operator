7a9edde23292d628657d2c1ec8669245e0615645
Jerry Seutter <jseutter@gmail.com>
2023-02-20 10:28:01 -0700

    Remove privileged container flag
    Signed-off-by: Jerry Seutter <jseutter@gmail.com>

--- pkg/operator/daemonset.go
+++ pkg/operator/daemonset.go
@@ -87,9 +87,6 @@ func MakeDaemonSet(fb fluentbitv1alpha2.FluentBit, logPath string) appsv1.Daemon
 							},
 							ReadinessProbe: fb.Spec.ReadinessProbe,
 							LivenessProbe:  fb.Spec.LivenessProbe,
-							SecurityContext: &corev1.SecurityContext{
-								Privileged: func(b bool) *bool { return &b }(true),
-							},
 							Env: []corev1.EnvVar{
 								{
 									Name: "NODE_NAME",
