5779839a3dc46d36a73145ecf0bbf130fa98c4d7
Elon Cheng <995729579@qq.com>
2023-07-04 20:10:17 +0800

    Update upgrade.sh

--- cmd/upgrade/upgrade.sh
+++ cmd/upgrade/upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 namespace=kubesphere-logging-system
-deployment="fluentbit-operator"
+FluentbitOperator="fluentbit-operator"
 
 function error_exit {
   echo "$1" 1>&2
@@ -9,20 +9,20 @@ function error_exit {
 
 function converting(){
 ## Converting an existing configuration to a new one
-local resource=$1
-local kind=$2
-local list=$(kubectl get  $resource.logging.kubesphere.io -A -o json) || error_exit "Cannot get resource $reresource"
+local oldKind=$1
+local newKind=$2
+local list=$(kubectl get  $oldKind.logging.kubesphere.io -A -o json) || error_exit "Cannot get resource $oldKind"
 local name=($(echo $list | jq -r '.items[].metadata.name | @json'))
 local labels=($(echo $list | jq -r '.items[].metadata.labels | @json'))
 local spec=($(echo $list | jq -r '.items[].spec | @json'))
 local ns=($(echo $list | jq -r '.items[].metadata.namespace | @json'))
 local size=${#spec[*]}
-echo "Number of original $resource configuration files:$size"
+echo "Number of original $oldKind configuration files:$size"
 for((i=0;i<${size};i++));do
 if [[ "${kind}" = "fluentbits" ]]; then
 cluster_resource_list[i]="{
 \"apiVersion\": \"fluentbit.fluent.io/v1alpha2\",
-\"kind\": \"${kind}\",
+\"kind\": \"${newKind}\",
 \"metadata\": {
 \"name\": ${name[i]},
 \"labels\": ${labels[i]},
@@ -48,32 +48,32 @@ for((i=0;i<${size};i++));do
 echo "${name[i]}"
   temp=$(echo ${name[i]} | sed 's/"//g')
   echo "$temp"
-   kubectl delete  $resource.logging.kubesphere.io $temp -n ${namespace}
+   kubectl delete  $oldKind.logging.kubesphere.io $temp -n ${namespace}
 done
 
 for((i=0;i<${size};i++));do
-echo ${cluster_resource_list[i]} | kubectl apply -f - || error_exit "Cannot apply resource $reresource"
+echo ${cluster_resource_list[i]} | kubectl apply -f - || error_exit "Cannot apply resource $oldKind"
 done
 }
 
-converting "inputs" "ClusterInput"
-converting "parsers" "ClusterParser"
-converting "filters" "ClusterFilter"
-converting "outputs" "ClusterOutput"
-converting "fluentbitconfigs" "ClusterFluentBitConfig"
-converting "fluentbits" "FluentBit"
+converting "Input" "ClusterInput"
+converting "Parser" "ClusterParser"
+converting "Filter" "ClusterFilter"
+converting "Output" "ClusterOutput"
+converting "FluentbitConfig" "ClusterFluentBitConfig"
+converting "FluentBit" "FluentBit"
 
 # Determine if Deployment exists
-if kubectl get deployment -n $namespace $deployment >/dev/null 2>&1; then
+if kubectl get deployment -n $namespace $FluentbitOperator >/dev/null 2>&1; then
     # Delete Deployment if it exists
-    kubectl delete deployment -n $namespace $deployment
+    kubectl delete deployment -n $namespace $FluentbitOperator
     kubectl delete clusterrolebinding kubesphere:operator:fluentbit-operator
     kubectl delete clusterrole kubesphere:operator:fluentbit-operator
     kubectl delete serviceaccount fluentbit-operator -n $namespace
-    echo "Deployment $deployment deleted"
+    echo "Deployment $FluentbitOperator deleted"
 else
     # If it does not exist, output the message
-    echo "Deployment $deployment does not exist"
+    echo "Deployment $FluentbitOperator does not exist"
 fi
 
 ## Delete the old crd
