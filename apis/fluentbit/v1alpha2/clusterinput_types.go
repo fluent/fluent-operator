0411c80df8a93ff5b71e586bd949c841b8f8ca70
“sjliu1” <sj_liu123@163.com>
2023-09-14 10:03:29 +0800

    [summerospp]add fluentbit nginx  plugin
    Signed-off-by: “sjliu1” <“sj_liu123@163.com”>

--- apis/fluentbit/v1alpha2/clusterinput_types.go
+++ apis/fluentbit/v1alpha2/clusterinput_types.go
@@ -63,7 +63,7 @@ type InputSpec struct {
 	MQTT *input.MQTT `json:"mqtt,omitempty"`
 	// Collectd defines the Collectd input plugin configuration
 	Collectd *input.Collectd `json:"collectd,omitempty"`
-	// Collectd defines the Collectd input plugin configuration
+	// Nginx defines the Nginx input plugin configuration
 	Nginx *input.Nginx `json:"nginx,omitempty"`
 }
 
