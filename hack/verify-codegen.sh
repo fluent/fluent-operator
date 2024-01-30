a30b20f1beb21be9d72e00981f30e905045afd8f
Markus Thömmes <markusthoemmes@me.com>
2024-01-30 15:02:10 +0100

    Adjust verify codegen
    Signed-off-by: Markus Thömmes <markusthoemmes@me.com>

--- hack/verify-codegen.sh
+++ hack/verify-codegen.sh
@@ -8,10 +8,6 @@ set -o pipefail
 
 SCRIPT_ROOT=$(git rev-parse --show-toplevel)
 
-# Grab code-generator version from go.sum.
-CODEGEN_VERSION=$(grep 'k8s.io/code-generator' go.sum | awk '{print $2}' | head -1 |cut -b 1-7)
-CODEGEN_PKG=$(echo `go env GOPATH`"/pkg/mod/k8s.io/code-generator@${CODEGEN_VERSION}")
-
 # code-generator does work with go.mod but makes assumptions about
 # the project living in `$GOPATH/src`. To work around this and support
 # any location; create a temporary directory, use this as an output
@@ -23,28 +19,18 @@ cleanup() {
 }
 trap "cleanup" EXIT SIGINT
 
-echo ">> Temporary output directory ${TEMP_DIR}"
-
-# Ensure we can execute.
-chmod +x ${CODEGEN_PKG}/generate-groups.sh
-
-${CODEGEN_PKG}/generate-groups.sh "client" \
-    github.com/fluent/fluent-operator/v2/apis/generated github.com/fluent/fluent-operator/v2/apis \
-    "fluentbit:v1alpha2 fluentd:v1alpha1" \
-    --output-base "${TEMP_DIR}" \
-    --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
-
-TMP_DIFFROOT=${TEMP_DIR}/github.com/fluent/fluent-operator/v2/apis/generated/clientset
 DIFFROOT=${SCRIPT_ROOT}/apis/generated/clientset
+cp -a "${DIFFROOT}"/* "${TEMP_DIR}"
 
-echo "diffing ${DIFFROOT} against freshly generated clientset"
+"${SCRIPT_ROOT}/hack/update-codegen.sh"
+echo "diffing ${DIFFROOT} against freshly generated codegen"
 ret=0
-diff -Naupr "${DIFFROOT}" "${TMP_DIFFROOT}" || ret=$?
-cp -a "${TMP_DIFFROOT}"/* "${DIFFROOT}"
+diff -Naupr "${DIFFROOT}" "${TEMP_DIR}" || ret=$?
+cp -a "${TEMP_DIR}"/* "${DIFFROOT}"
 if [[ $ret -eq 0 ]]
 then
   echo "${DIFFROOT} up to date."
 else
-  echo "${DIFFROOT} is out of date. Please rerun make generate"
+  echo "${DIFFROOT} is out of date. Please run hack/update-codegen.sh"
   exit 1
 fi
