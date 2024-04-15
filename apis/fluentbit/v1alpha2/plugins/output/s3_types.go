92605d5d1e58f31eb056bc3a43167994f496858b
Jeff Evans <jevans@confluent.io>
2024-04-15 15:31:57 -0500

    Add Profile field for Fluent bit cluster output
    Signed-off-by: Jeff Evans <jevans@confluent.io>

--- apis/fluentbit/v1alpha2/plugins/output/s3_types.go
+++ apis/fluentbit/v1alpha2/plugins/output/s3_types.go
@@ -63,7 +63,9 @@ type S3 struct {
 	// Integer value to set the maximum number of retries allowed.
 	RetryLimit *int32 `json:"RetryLimit,omitempty"`
 	// Specify an external ID for the STS API, can be used with the role_arn parameter if your role requires an external ID.
-	ExternalId   string `json:"ExternalId,omitempty"`
+	ExternalId string `json:"ExternalId,omitempty"`
+	// Option to specify an AWS Profile for credentials.
+	Profile      string `json:Profile,omitempty`
 	*plugins.TLS `json:"tls,omitempty"`
 }
 
@@ -153,6 +155,9 @@ func (o *S3) Params(sl plugins.SecretLoader) (*params.KVs, error) {
 	}
 	if o.ExternalId != "" {
 		kvs.Insert("external_id", o.ExternalId)
+	}
+	if o.Profile != "" {
+		kvs.Insert("profile", o.Profile)
 	}
 	if o.TLS != nil {
 		tls, err := o.TLS.Params(sl)
