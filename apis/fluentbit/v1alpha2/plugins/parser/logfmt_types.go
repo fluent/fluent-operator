ed5a27b79605ae2070dd40cf4bdfd73a14fd42ca
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-06-05 07:47:49 -0400

    Update logfmt_types.go
    Signed-off-by: Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>

--- apis/fluentbit/v1alpha2/plugins/parser/logfmt_types.go
+++ apis/fluentbit/v1alpha2/plugins/parser/logfmt_types.go
@@ -9,12 +9,29 @@ import (
 
 // The logfmt parser allows to parse the logfmt format described in https://brandur.org/logfmt . <br />
 // **For full documentation, refer to https://docs.fluentbit.io/manual/pipeline/parsers/logfmt**
-type Logfmt struct{}
+type Logfmt struct{
+	// Time_Key
+	TimeKey string `json:"timeKey,omitempty"`
+	// Time_Format, eg. %Y-%m-%dT%H:%M:%S %z
+	TimeFormat string `json:"timeFormat,omitempty"`
+	// Time_Keep
+	TimeKeep *bool `json:"timeKeep,omitempty"`
+}
 
 func (_ *Logfmt) Name() string {
 	return "logfmt"
 }
 
-func (_ *Logfmt) Params(_ plugins.SecretLoader) (*params.KVs, error) {
-	return nil, nil
+func (j *Logfmt) Params(_ plugins.SecretLoader) (*params.KVs, error) {
+	kvs := params.NewKVs()
+	if l.TimeKey != "" {
+		kvs.Insert("Time_Key", l.TimeKey)
+	}
+	if l.TimeFormat != "" {
+		kvs.Insert("Time_Format", l.TimeFormat)
+	}
+	if l.TimeKeep != nil {
+		kvs.Insert("Time_Keep", fmt.Sprint(*l.TimeKeep))
+	}
+	return kvs, nil
 }
