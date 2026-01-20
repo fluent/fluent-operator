b434305e59862cd5ff350c6e30057d48b1d90d2f
Kilian Ries <kr@opusdns.com>
2026-01-20 10:38:19 +0100

    Add more parameters to opensearch output plugin (#1785)
    * add more parameters to opensearch output plugin
    
    Signed-off-by: Kilian Ries <mail@kilian-ries.de>
    
    * refactor
    
    Signed-off-by: Kilian Ries <mail@kilian-ries.de>
    
    * refactor
    
    Signed-off-by: Kilian Ries <mail@kilian-ries.de>
    
    * Regenerate opensearch plugin documentation
    
    Signed-off-by: kilian.ries <kr@opusdns.com>
    
    ---------
    
    Signed-off-by: Kilian Ries <mail@kilian-ries.de>
    Signed-off-by: kilian.ries <kr@opusdns.com>

--- apis/fluentd/v1alpha1/plugins/output/opensearch.go
+++ apis/fluentd/v1alpha1/plugins/output/opensearch.go
@@ -28,4 +28,95 @@ type Opensearch struct {
 	Password *plugins.Secret `json:"password,omitempty"`
 	// Optional, Force certificate validation
 	SslVerify *bool `json:"sslVerify,omitempty"`
+	// Optional, Absolute path to CA certificate file
+	CAFile *string `json:"caFile,omitempty"`
+	// Optional, Absolute path to client Certificate file
+	ClientCert *string `json:"clientCert,omitempty"`
+	// Optional, Absolute path to client private Key file
+	ClientKey *string `json:"clientKey,omitempty"`
+	// Optional, password for ClientKey file
+	ClientKeyPassword *plugins.Secret `json:"clientKeyPassword,omitempty"`
+	// Optional, You can specify SSL/TLS version (default: TLSv1_2)
+	SslVersion *string `json:"sslVersion,omitempty"`
+	// Optional, Minimum SSL/TLS version
+	SslMinVersion *string `json:"sslMinVersion,omitempty"`
+	// Optional, Maximum SSL/TLS version
+	SslMaxVersion *string `json:"sslMaxVersion,omitempty"`
+	// Optional, Enable logging of 400 reason without enabling debug log level (default: false)
+	LogOs400Reason *bool `json:"logOs400Reason,omitempty"`
+	// Optional, HTTP request timeout in seconds (default: 5s)
+	// +kubebuilder:validation:Pattern:="^\\d+(s|m|h|d)$"
+	RequestTimeout *string `json:"requestTimeout,omitempty"`
+	// Optional, Indicates that the plugin should reset connection on any error (reconnect on next send) (default: false)
+	ReconnectOnError *bool `json:"reconnectOnError,omitempty"`
+	// Optional, Automatically reload connection after 10000 documents (default: true)
+	ReloadConnections *bool `json:"reloadConnections,omitempty"`
+	// Optional, When ReloadConnections true, this is the integer number of operations after which the plugin will reload the connections (default: 10000)
+	ReloadAfter *uint32 `json:"reloadAfter,omitempty"`
+	// Optional, Indicates that the opensearch-transport will try to reload the nodes addresses if there is a failure while making the request (default: false)
+	ReloadOnFailure *bool `json:"reloadOnFailure,omitempty"`
+	// Optional, You can specify times of retry obtaining OpenSearch version (default: 15)
+	MaxRetryGetOsVersion *uint32 `json:"maxRetryGetOsVersion,omitempty"`
+	// Optional, Indicates whether to fail when max_retry_get_os_version is exceeded (default: true)
+	FailOnDetectingOsVersionRetryExceed *bool `json:"failOnDetectingOsVersionRetryExceed,omitempty"`
+	// Optional, Default OpenSearch version (default: 1)
+	DefaultOpensearchVersion *uint32 `json:"defaultOpensearchVersion,omitempty"`
+	// Optional, Validate OpenSearch version at startup (default: true)
+	VerifyOsVersionAtStartup *bool `json:"verifyOsVersionAtStartup,omitempty"`
+	// Optional, Always update the template, even if it already exists (default: false)
+	TemplateOverwrite *bool `json:"templateOverwrite,omitempty"`
+	// Optional, You can specify times of retry putting template (default: 10)
+	MaxRetryPuttingTemplate *uint32 `json:"maxRetryPuttingTemplate,omitempty"`
+	// Optional, Indicates whether to fail when max_retry_putting_template is exceeded (default: true)
+	FailOnPuttingTemplateRetryExceed *bool `json:"failOnPuttingTemplateRetryExceed,omitempty"`
+	// Optional, Provide a different sniffer class name
+	SnifferClassName *string `json:"snifferClassName,omitempty"`
+	// Optional, Provide a selector class name
+	SelectorClassName *string `json:"selectorClassName,omitempty"`
+	// Optional, You can specify HTTP backend (default: excon). Options: excon, typhoeus
+	HttpBackend *string `json:"httpBackend,omitempty"`
+	// Optional, With http_backend_excon_nonblock false, plugin uses excon with nonblock=false (default: true)
+	HttpBackendExconNonblock *bool `json:"httpBackendExconNonblock,omitempty"`
+	// Optional, You can specify the compression level (default: no_compression). Options: no_compression, best_compression, best_speed, default_compression
+	CompressionLevel *string `json:"compressionLevel,omitempty"`
+	// Optional, With default behavior, plugin uses Yajl as JSON encoder/decoder. Set to true to use Oj (default: false)
+	PreferOjSerializer *bool `json:"preferOjSerializer,omitempty"`
+	// Optional, Suppress '[types removal]' warnings on OpenSearch 2.x (default: true for OS2+)
+	SuppressTypeName *bool `json:"suppressTypeName,omitempty"`
+	// Optional, With content_type application/x-ndjson, plugin adds application/x-ndjson as Content-Type (default: application/json)
+	ContentType *string `json:"contentType,omitempty"`
+	// Optional, Include tag key in record (default: false)
+	IncludeTagKey *bool `json:"includeTagKey,omitempty"`
+	// Optional, Tag key name when include_tag_key is true (default: tag)
+	TagKey *string `json:"tagKey,omitempty"`
+	// Optional, Record accessor syntax to specify the field to use as _id in OpenSearch
+	IdKey *string `json:"idKey,omitempty"`
+	// Optional, Remove specified keys from the event record
+	RemoveKeys *string `json:"removeKeys,omitempty"`
+	// Optional, Remove keys when record is being updated
+	RemoveKeysOnUpdate *string `json:"removeKeysOnUpdate,omitempty"`
+	// Optional, The write operation (default: index). Options: index, create, update, upsert
+	WriteOperation *string `json:"writeOperation,omitempty"`
+	// Optional, When write_operation is not index, setting this true will cause plugin to emit_error_event of records which do not include _id field (default: false)
+	EmitErrorForMissingId *bool `json:"emitErrorForMissingId,omitempty"`
+	// Optional, Custom headers in Hash format
+	CustomHeaders *string `json:"customHeaders,omitempty"`
+	// Optional, Pipeline name
+	Pipeline *string `json:"pipeline,omitempty"`
+	// Optional, UTC index (default: false for local time)
+	UtcIndex *bool `json:"utcIndex,omitempty"`
+	// Optional, Suppress doc_wrap (default: false)
+	SuppressDocWrap *bool `json:"suppressDocWrap,omitempty"`
+	// Optional, List of exception classes to ignore
+	IgnoreExceptions *string `json:"ignoreExceptions,omitempty"`
+	// Optional, Backup chunk when ignore exception occurs (default: true)
+	ExceptionBackup *bool `json:"exceptionBackup,omitempty"`
+	// Optional, Configure bulk_message request splitting threshold size (default: -1 unlimited)
+	BulkMessageRequestThreshold *int32 `json:"bulkMessageRequestThreshold,omitempty"`
+	// Optional, Specify the application name for the rollover index to be created (default: default)
+	ApplicationName *string `json:"applicationName,omitempty"`
+	// Optional, Specify the index date pattern for creating a rollover index (default: now/d)
+	IndexDatePattern *string `json:"indexDatePattern,omitempty"`
+	// Optional, Use legacy template or not (default: false for composable templates)
+	UseLegacyTemplate *bool `json:"useLegacyTemplate,omitempty"`
 }
--- docs/plugins/fluentd/output/opensearch.md
+++ docs/plugins/fluentd/output/opensearch.md
@@ -16,3 +16,48 @@ Opensearch defines the parameters for out_opensearch plugin
 | user | Optional, The login credentials to connect to Opensearch | *[plugins.Secret](../secret.md) |
 | password | Optional, The login credentials to connect to Opensearch | *[plugins.Secret](../secret.md) |
 | sslVerify | Optional, Force certificate validation | *bool |
+| caFile | Optional, Absolute path to CA certificate file | *string |
+| clientCert | Optional, Absolute path to client Certificate file | *string |
+| clientKey | Optional, Absolute path to client private Key file | *string |
+| clientKeyPassword | Optional, password for ClientKey file | *[plugins.Secret](../secret.md) |
+| sslVersion | Optional, You can specify SSL/TLS version (default: TLSv1_2) | *string |
+| sslMinVersion | Optional, Minimum SSL/TLS version | *string |
+| sslMaxVersion | Optional, Maximum SSL/TLS version | *string |
+| logOs400Reason | Optional, Enable logging of 400 reason without enabling debug log level (default: false) | *bool |
+| requestTimeout | Optional, HTTP request timeout in seconds (default: 5s) | *string |
+| reconnectOnError | Optional, Indicates that the plugin should reset connection on any error (reconnect on next send) (default: false) | *bool |
+| reloadConnections | Optional, Automatically reload connection after 10000 documents (default: true) | *bool |
+| reloadAfter | Optional, When ReloadConnections true, this is the integer number of operations after which the plugin will reload the connections (default: 10000) | *uint32 |
+| reloadOnFailure | Optional, Indicates that the opensearch-transport will try to reload the nodes addresses if there is a failure while making the request (default: false) | *bool |
+| maxRetryGetOsVersion | Optional, You can specify times of retry obtaining OpenSearch version (default: 15) | *uint32 |
+| failOnDetectingOsVersionRetryExceed | Optional, Indicates whether to fail when max_retry_get_os_version is exceeded (default: true) | *bool |
+| defaultOpensearchVersion | Optional, Default OpenSearch version (default: 1) | *uint32 |
+| verifyOsVersionAtStartup | Optional, Validate OpenSearch version at startup (default: true) | *bool |
+| templateOverwrite | Optional, Always update the template, even if it already exists (default: false) | *bool |
+| maxRetryPuttingTemplate | Optional, You can specify times of retry putting template (default: 10) | *uint32 |
+| failOnPuttingTemplateRetryExceed | Optional, Indicates whether to fail when max_retry_putting_template is exceeded (default: true) | *bool |
+| snifferClassName | Optional, Provide a different sniffer class name | *string |
+| selectorClassName | Optional, Provide a selector class name | *string |
+| httpBackend | Optional, You can specify HTTP backend (default: excon). Options: excon, typhoeus | *string |
+| httpBackendExconNonblock | Optional, With http_backend_excon_nonblock false, plugin uses excon with nonblock=false (default: true) | *bool |
+| compressionLevel | Optional, You can specify the compression level (default: no_compression). Options: no_compression, best_compression, best_speed, default_compression | *string |
+| preferOjSerializer | Optional, With default behavior, plugin uses Yajl as JSON encoder/decoder. Set to true to use Oj (default: false) | *bool |
+| suppressTypeName | Optional, Suppress '[types removal]' warnings on OpenSearch 2.x (default: true for OS2+) | *bool |
+| contentType | Optional, With content_type application/x-ndjson, plugin adds application/x-ndjson as Content-Type (default: application/json) | *string |
+| includeTagKey | Optional, Include tag key in record (default: false) | *bool |
+| tagKey | Optional, Tag key name when include_tag_key is true (default: tag) | *string |
+| idKey | Optional, Record accessor syntax to specify the field to use as _id in OpenSearch | *string |
+| removeKeys | Optional, Remove specified keys from the event record | *string |
+| removeKeysOnUpdate | Optional, Remove keys when record is being updated | *string |
+| writeOperation | Optional, The write operation (default: index). Options: index, create, update, upsert | *string |
+| emitErrorForMissingId | Optional, When write_operation is not index, setting this true will cause plugin to emit_error_event of records which do not include _id field (default: false) | *bool |
+| customHeaders | Optional, Custom headers in Hash format | *string |
+| pipeline | Optional, Pipeline name | *string |
+| utcIndex | Optional, UTC index (default: false for local time) | *bool |
+| suppressDocWrap | Optional, Suppress doc_wrap (default: false) | *bool |
+| ignoreExceptions | Optional, List of exception classes to ignore | *string |
+| exceptionBackup | Optional, Backup chunk when ignore exception occurs (default: true) | *bool |
+| bulkMessageRequestThreshold | Optional, Configure bulk_message request splitting threshold size (default: -1 unlimited) | *int32 |
+| applicationName | Optional, Specify the application name for the rollover index to be created (default: default) | *string |
+| indexDatePattern | Optional, Specify the index date pattern for creating a rollover index (default: now/d) | *string |
+| useLegacyTemplate | Optional, Use legacy template or not (default: false for composable templates) | *bool |
