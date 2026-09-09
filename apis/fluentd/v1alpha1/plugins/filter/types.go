8cfff90593b668ed586c5702dbb33944d10faef6
Marco Franssen <marco.franssen@gmail.com>
2026-09-09 17:14:31 +0200

    Apply suggestion from @marcofranssen
    Signed-off-by: Marco Franssen <marco.franssen@gmail.com>

--- apis/fluentd/v1alpha1/plugins/filter/types.go
+++ apis/fluentd/v1alpha1/plugins/filter/types.go
@@ -22,7 +22,7 @@ type FilterCommon struct {
 // Filter defines all available filter plugins and their parameters.
 type Filter struct {
 	// The common fields
-	FilterCommon `json:",inline"`
+	FilterCommon `json:",inline,omitempty"`
 
 	// The filter_grep filter plugin
 	Grep *Grep `json:"grep,omitempty"`
